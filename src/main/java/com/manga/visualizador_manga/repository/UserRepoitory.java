470c62f8bb9230bf38228c858c0f7a33f950b3c3
Marianannn <mariana.leticia05@gmail.com>
2025-06-06 21:57:38 -0300

    feature: adicionando repositorio de usuário

---
+++ src/main/java/com/manga/visualizador_manga/repository/UserRepoitory.java
@@ -0,0 +1,16 @@
+package com.manga.visualizador_manga.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.manga.visualizador_manga.model.Usuario;
+
+public interface UserRepoitory extends JpaRepository<Usuario, Integer>{
+    @Query("SELECT u FROM Usuario u WHERE u.id = :id")
+        Optional<Usuario> findById(@Param("id") Integer id);
+
+        boolean existsByNome(String nome);
+}

747db0a04aa333b4a0c80f2ac2e7668643d033ee
Marianannn <mariana.leticia05@gmail.com>
2025-06-06 22:52:50 -0300

    feature: crição do repositorio de usuario

--- src/main/java/com/manga/visualizador_manga/repository/UsuarioRepository.java
+++ src/main/java/com/manga/visualizador_manga/repository/UsuarioRepository.java
@@ -4,13 +4,11 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.manga.visualizador_manga.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
     @Query("SELECT u FROM Usuario u WHERE u.id = :id")
-        Optional<Usuario> findById(@Param("id") Integer id);
 
-        boolean existsByNome(String nome);
+        boolean existsByEmail(String email);
 }
---
+++ src/main/java/com/manga/visualizador_manga/service/impl/UsuarioServiceImpl.java
@@ -0,0 +1,69 @@
+package com.manga.visualizador_manga.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import com.manga.visualizador_manga.model.Usuario;
+import com.manga.visualizador_manga.repository.UsuarioRepository;
+import com.manga.visualizador_manga.service.UsuarioService;
+import com.manga.visualizador_manga.service.exception.BusinessException;
+import com.manga.visualizador_manga.service.exception.NotFoundException;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService{
+    
+    private final UsuarioRepository repository;
+
+    public UsuarioServiceImpl(UsuarioRepository repository){
+        this.repository = repository;
+    }
+
+    public List<Usuario> findAll(){
+        return repository.findAll();
+    }
+
+    public Usuario findById(Integer id){
+        return repository.findById(id).orElseThrow(() -> new NotFoundException());
+    }
+
+    public Usuario create(Usuario usuario){
+        
+        if(usuario.getNome() == null || usuario.getNome().isEmpty()){
+            throw new BusinessException("Nome do usuário não pode ser nulo!!!");
+        }
+        if(usuario.getEmail() == null || usuario.getEmail().isEmpty()){
+            throw new BusinessException("Email do usuário não pode ser nulo!!!");
+        }
+        if(usuario.getSenha() == null || usuario.getSenha().isEmpty()){
+            throw new BusinessException("Senha não pode ser nula!!!");
+        }
+
+        if(repository.existsByEmail(usuario.getEmail())){
+            throw new BusinessException("Email já cadastrado!");
+        }
+        return repository.save(usuario);
+    }
+
+    public Usuario update(Integer id, Usuario usuario){
+        Usuario usuarioBanco = this.findById(id);
+        if(!usuarioBanco.getId().equals(usuarioBanco.getId())){
+            throw new BusinessException("id de atualização precisa ser o mesmo.");
+        }
+
+        usuarioBanco.setNome(usuario.getNome());
+        usuarioBanco.setEmail(usuario.getEmail());
+        usuarioBanco.setSenha(usuario.getSenha());
+        return repository.save(usuarioBanco);
+    }
+
+    public void delete(Integer id){
+        Usuario usuarioBanco = this.findById(id);
+        this.repository.delete(usuarioBanco);
+    }
+
+    public Boolean existsByEmail(String email){
+        Boolean exists = repository.existsByEmail(email);
+        return exists;
+    }
+    
+}

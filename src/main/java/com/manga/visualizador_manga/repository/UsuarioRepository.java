8ad75eb21393f2229b7687a04b5588a3702ae4b8
Marianannn <mariana.leticia05@gmail.com>
2025-06-06 22:00:25 -0300

    fix: alterando nome do repository de usuario

--- src/main/java/com/manga/visualizador_manga/repository/UsuarioRepository.java
+++ src/main/java/com/manga/visualizador_manga/repository/UsuarioRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 
 import com.manga.visualizador_manga.model.Usuario;
 
-public interface UserRepoitory extends JpaRepository<Usuario, Integer>{
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer>{
     @Query("SELECT u FROM Usuario u WHERE u.id = :id")
         Optional<Usuario> findById(@Param("id") Integer id);
 
